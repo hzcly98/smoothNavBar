ccbca10a6be009a6daa702df53e56031ea836d09
hzcly98 <hzcly982015@163.com>
2017-11-24 09:36:01 +0800

    Initial Commit

---
+++ tableviewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableviewTest
+//
+//  Created by hzc on 2017/11/24.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableviewTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tableviewTest
+//
+//  Created by hzc on 2017/11/24.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
